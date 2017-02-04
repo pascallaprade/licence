85edd0404ed77d29b7851bfa158734db222fc351
Pascal Laprade <laprade.p@gmail.com>
2017-02-03 22:56:02 -0500

    Updated usage for multiple output support
    - The usage message now shows that the user can specify multiple
      output files at the end of the command.

--- src/errors.c
+++ src/errors.c
@@ -1,16 +1,16 @@
 /* licence : a tool for prepending a license to files of a project.
  * Copyright (C) 2016  Pascal Laprade <laprade.p@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
@@ -23,7 +23,7 @@
 /** Displays an error message in case of missing args. */
 void error_not_enough_args()
 {
-    fprintf(stderr, "Usage: licence -l <LICENCE> [-o] <FILE>\n");
+    fprintf(stderr, "Usage: licence -l <LICENCE> [-o] <FILE>...\n");
 }
 
 /** Displays an error message in case of a missing licence arg. */
